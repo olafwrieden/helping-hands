1e69ca3bfdf322f070e0e8fd029c95801e31e8c3
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-04-18 18:15:09 +1200

    Added Register Form

---
+++ client/src/components/Register/components/AddressDetails.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const AddressDetails = ({
+  setStep,
+  setStreetAddress,
+  setSuburb,
+  setCity,
+  submit,
+}) => {
+  return (
+    <div className="container">
+      <div className="field">
+        <label className="label">Street</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="1 Example Way"
+            name="street"
+            id="street"
+            onChange={(e) => setStreetAddress(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Suburb</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Orakei"
+            name="suburb"
+            id="suburb"
+            onChange={(e) => setSuburb(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">City</label>
+        <div className="control">
+          <input
+            className="input"
+            name="City"
+            type="City"
+            placeholder="Auckland"
+            id="form-email"
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="field is-grouped">
+        <div className="control">
+          <button className="button" onClick={() => setStep(0)}>
+            <span className="icon is-small">
+              <i className="fas fa-arrow-left" aria-hidden="true"></i>
+            </span>
+            <span>Back</span>
+          </button>
+        </div>
+        <div className="control">
+          <button className="button is-success" onClick={submit}>
+            <span className="icon is-small">
+              <i className="fas fa-check" aria-hidden="true"></i>
+            </span>
+            <span>Register</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressDetails;

f9a3417b97ab42d44e7b6245139b108d166c4206
HHSNZ <hh.stobo@gmail.com>
2020-04-18 20:57:35 +1200

    able to move buddies from accepted to pending. In future this will be replaced by api calls.

--- client/src/components/BuddyList/index.js
+++ client/src/components/BuddyList/index.js
@@ -4,7 +4,7 @@ import "./BuddyList.css";
 const BuddyList = () => {
     const [buddies, setBuddies] = useState([
         {
-            id: "pk 1",
+            id: 1,
             avatar_image: "https://cdn.pixabay.com/photo/2016/11/13/21/46/sheep-1822137_960_720.jpg",
             first_name: "Peter",
             last_name: "Parker",
@@ -13,7 +13,7 @@ const BuddyList = () => {
             phone: "515-2234"
         },
         {
-            id: "pk 2",
+            id: 2,
             avatar_image: "https://cdn.pixabay.com/photo/2016/10/21/19/45/hedgehog-child-1759027_960_720.jpg",
             first_name: "Harry",
             last_name: "Potter",
@@ -22,7 +22,7 @@ const BuddyList = () => {
             phone: "114-9181"
         },
         {   
-            id: "pk 3",
+            id: 3,
             avatar_image: "https://cdn.pixabay.com/photo/2016/01/05/17/51/dog-1123016__340.jpg",
             first_name: "Doctor",
             last_name: "Dolitte",
@@ -32,12 +32,41 @@ const BuddyList = () => {
         }
     ])
 
-    useEffect(() => {
-        //this will call the api to retrieve buddy list, including user info about each buddy.
-    })
+    // useEffect(() => {
+    //     // this will call the api to retrieve buddy list, including user info about each buddy.
+    // }, [])
 
-    //when pending buddy is accepted, status is changed from pending to accepted,
-    //a call is made to the api to update the buddy in that row
+    //this function puts items in pending when remove icon clicked, and puts
+    //them in accepted when accept icon is clicked
+    const updateBuddyList = (id, action) => {
+        switch(action) {
+            case "accept":
+                setBuddies(buddies.map((item, idx) => {
+                    if(item.id === id) {
+                        return {...item, status: "accepted"}
+                    } else {
+                        return item
+                    }
+                }))
+                //go to api and update status at pk of id to accepted
+                //update component state with new buddy list in db
+                //try catch to handle error
+                break;
+            case "remove":
+                console.log('this user would like to ', action, ' this buddy request or buddy')
+                setBuddies(buddies.map((item, idx) => {
+                    if(item.id === id) {
+                        return {...item, status: "pending"}
+                    } else {
+                        return item
+                    }
+                }))
+                //go to api and remove row where pk equals id
+                //update component state with new buddy list in db
+                //try catch to handle error
+                break;
+        }
+    }
     return (
         <div className="buddylist-container">
         <h2 className="title" >Your Buddies</h2>
@@ -52,11 +81,11 @@ const BuddyList = () => {
                             <img className="level-left" width="50" src={item.avatar_image} alt="buddy avatar picture" />
                             <span className="full-name"><strong>{`${item.first_name} ${item.last_name}`}</strong></span> 
                             <span className="status">Would like to buddy up with you</span>
-                            <span class="icon is-medium has-text-success">
-                                <i class="fas fa-check-square fa-lg"></i>
+                            <span className="icon is-medium has-text-success" onClick={() => updateBuddyList(item.id, "accept")}>
+                                <i className="fas fa-check-square fa-lg"></i>
                             </span>
-                            <span class="icon is-medium has-text-danger">
-                                <i class="fas fa-ban fa-lg"></i>
+                            <span className="icon is-medium has-text-danger" onClick={() => updateBuddyList(item.id, "remove")}>
+                                <i className="fas fa-ban fa-lg"></i>
                             </span>
                         </div>
                         </>
@@ -73,9 +102,12 @@ const BuddyList = () => {
                         <div key={`${item.first_name} ${idx}`} className="level card is-mobile buddylist-item">
                             <img className="level-left" width="50" src={item.avatar_image} alt="buddy avatar picture" />
                             <span className="full-name"><strong>{`${item.first_name} ${item.last_name}`}</strong></span> 
-                            <span className="status"><i class="fas fa-home fa-lg"></i>{item.address}<br /> <i class="fas fa-phone fa-lg"></i> {item.phone} </span>
-                            <span class="icon is-medium has-text-danger accepted-del-btn">
-                                <i class="fas fa-ban fa-lg"></i>
+                            <div className="details">
+                                <span><i className="fas fa-home fa-lg"></i>{item.address}</span> <br />
+                                <span><i className="fas fa-phone fa-lg"></i> {item.phone} </span>
+                            </div>
+                            <span className="icon is-medium has-text-danger accepted-del-btn" onClick={() => updateBuddyList(item.id, "remove")}>
+                                <i className="fas fa-ban fa-lg"></i>
                             </span>
                         </div>
                         </>

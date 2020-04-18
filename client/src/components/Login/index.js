af476ae443fc3da9b4b9e79980c16eb107078d26
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-04-19 01:15:28 +1200

    Added Logout

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -86,18 +86,36 @@ const Login = ({ history }) => {
   );
 };
 
-export const LogOut = () => (
-  <section className="section">
-    <div className="container">
-      <div className="columns login-hero">
-        <div className="column is-1 level flat"></div>
-        <div className="column is-5 level">
-          <h1 className="title is-1 login-hero-title">Log Out</h1>
-          <h2 className="subtitle is-3">This feature is not available.</h2>
+export const LogOut = ({ history }) => {
+  fetch("/api/v1/logout", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return history.push("/");
+      }
+    })
+    .catch((err) => {
+      return history.push("/");
+    });
+
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns login-hero">
+          <div className="column is-1 level flat"></div>
+          <div className="column is-5 level">
+            <h1 className="title is-1 login-hero-title">Logging Out</h1>
+            <h2 className="subtitle is-3">This feature is not available.</h2>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Login;

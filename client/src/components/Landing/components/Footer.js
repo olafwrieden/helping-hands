1866dc7d9da09568a301fd3494285634c9aeec73
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-04-20 00:12:50 +1200

    Linked GitHub

--- client/src/components/Landing/components/Footer.js
+++ client/src/components/Landing/components/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => (
   <footer className="footer">
     <div className="container">
       <p className="subtitle has-text-centered is-6">
-        &copy; 2020 Helping Hands. All right reserved.
+        &copy; 2020 All right reserved.{" "}
+        <a href="https://github.com/olafwrieden/helping-hands" target="_blank">
+          View on GitHub
+        </a>
       </p>
     </div>
   </footer>

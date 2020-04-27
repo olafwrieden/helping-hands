d0006bbc0a4be8eb4f50ae5f45acd1073ef1f767
HHSNZ <hh.stobo@gmail.com>
2020-04-27 14:41:43 +1200

    frontend coming together for my requests page for 'in-need' users of the app. Need to implement decent looking animations for showing/hiding more details div, and connect to backend.

--- client/src/components/myrequests/index.js
+++ client/src/components/myrequests/index.js
@@ -1,55 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MyRequests.css'
 
 const MyRequests = () => {
+    const [clickedIndex, setClickedIndex] = useState(null)
     const requestsData = [
         {
             type: "Assistance",
             requestedAt: "08:00",
             status: "Accepted",
-            acceptedBy: "Tom Hardy"
+            acceptedBy: "Tom Hardy",
+            details: "I need assistance mowing my lawn please.",
+            address: "3 Example Street",
+            city: "Auckland"
         },
         {
             type: "Pickup",
             requestedAt: "10:00",
             status: "Pending",
-            acceptedBy: ""
+            acceptedBy: "N/A",
+            details: "I need 3 carrots, 4 onions and a pumpkin from the grocery store.",
+            address: "5 Test Drive",
+            city: "Auckland"
         },
         {
             type: "Assistance",
             requestedAt: "09:00",
             status: "Pending",
-            acceptedBy: ""
+            acceptedBy: "N/A",
+            details: "I need my gutters cleaned out.",
+            address: "21 Jump Street",
+            city: "Auckland"
         },
         {
             type: "Third Party Assistance",
             requestedAt: "11:30",
             status: "Accepted",
-            acceptedBy: ""
+            acceptedBy: "Jemma Schofield",
+            details: "I am requesting assistance on behalf of my sick Aunt, please pick up some panadol for her.",
+            address: "10 Dawning Street",
+            city: "Auckland"
         }
     ]
+
+    const showDivFunc = index => {
+        return clickedIndex === index 
+        ? setClickedIndex(null)
+        : setClickedIndex(index)
+    }
 
     return (
         <div className="requests-container card">
             <h2 className="title is-3">My Requests</h2>
             <div className="card">
-                <div className="level columns is-mobile">
-                    <span className="column">Type</span>
-                    <span className="column">Requested At</span>
-                    <span className="column">Status</span>
-                    <span className="column">Accepted By</span>
-                    <span className="column is-narrow"> </span>
+                <div className="level columns is-mobile req-title-row is-marginless is-paddingless">
+                    <span className="column is-one-quarter has-text-weight-bold">Type</span>
+                    <span className="column is-one-quarter has-text-weight-bold">Requested At</span>
+                    <span className="column is-one-quarter has-text-weight-bold">Status</span>
+                    <span className="column is-one-quarter has-text-weight-bold">Accepted By</span>
                 </div>
                 {requestsData.map((item, idx) => 
-                    <button key={`${item}${idx}`} className="button columns is-mobile req-list">
-                        <span className="column">{item.type}</span>
-                        <span className="column">{item.requestedAt}</span>
-                        <span className="column">{item.status}</span>
-                        <span className="column">{item.acceptedBy}</span>
-                        <button className="column is-narrow">
-                            <i></i>
-                        </button>
-                    </button>
+                <div>
+                <button onClick={() => showDivFunc(idx)} key={`${item}${idx}`} className="button columns is-mobile req-list-item is-marginless is-paddingless">
+                    <span className="column is-one-quarter">{item.type}</span>
+                    <span className="column is-one-quarter">{item.requestedAt}</span>
+                    <span className="column is-one-quarter">{item.status}</span>
+                    <span className="column is-one-quarter">{item.acceptedBy}</span>
+                </button>
+                <div className={`req-item-popout ${clickedIndex === idx ? "show-req-details" : "hide-req-details"}`}>
+                    <div className="level columns is-mobile">
+                    <span className="column is-one-quarter has-text-weight-bold has-text-centered">Details: </span>
+                    <p className="column is-three-quarters has-text-left">{item.details}</p>
+                    </div>
+                    <div className="level columns is-mobile">
+                    <span className="column is-one-quarter has-text-weight-bold has-text-centered">Address: </span>
+                    <p className="column is-three-quarters has-text-left">{item.address}</p>
+                    </div>
+                </div>
+                </div>
                 )}
             </div> 
         </div>

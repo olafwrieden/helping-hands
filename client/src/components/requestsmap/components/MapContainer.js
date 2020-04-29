b1075f16c9f5b25d9c6940975ed89521561b8901
HHSNZ <hh.stobo@gmail.com>
2020-04-29 15:08:29 +1200

    map displaying with pin, geocoder working

--- client/src/components/requestsmap/components/MapContainer.js
+++ client/src/components/requestsmap/components/MapContainer.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../RequestsMap.css'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import { useAuth } from "../../App/Authentication";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+import { useAuth } from '../../App/Authentication'
+import Geocode from 'react-geocode'
+Geocode.setApiKey('AIzaSyALJlUnHvzueMf92A0kFkVnxl24GW_xTDU')
 
 
 const style = {
@@ -10,24 +12,50 @@ const style = {
     transform: 'TranslateX(0)'
   }
 
-const MapContainer = (props) => {
-    const { isAuthed, user } = useAuth();
-    const address = user.address
+const MapContainer = ({pinsArr, google}) => {
+    const { user } = useAuth();
+    const {address, city, zipCode} = user
+    const [userCoordinates, setUserCoordinates] = useState({initLat: null, initLng: null})
+    const [coordsArray, setCoordsArray] = useState([])
+    const { initLat, initLng } = userCoordinates
     //geocode address to lat/long coordinates
     //placeholder:
-    const akl = {
-        lat: -36.848461,
-        long: 174.763336
-    }
+    useEffect(() => {
+        const geoCoder = async () => {
+            try {
+            const fetchGeocode = await Geocode.fromAddress(`${address}, ${city}, ${zipCode}`)
+            const { lat, lng } = fetchGeocode.results[0].geometry.location
+            setUserCoordinates({initLat: lat, initLng: lng})
+            const requestsCoords = pinsArr.map(async item => {
+                const fetchGeocoder = await Geocode.fromAddress(`${address}, ${city}, ${zipCode}`)
+                const { lat, lng } = fetchGeocoder.results[0].geometry.location
+                return item.coords = {lat, lng}
+            }) 
+            setCoordsArray(requestsCoords)
+
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        geoCoder()
+    }, [])
+    
     return (
-        <Map 
-            google={props.google} 
-            zoom={13}  
-            initialCenter={{lat: akl.lat, lng: akl.long}}
+        <>
+        {initLat && <Map 
+            google={google} 
+            zoom={14}  
+            initialCenter={{lat: initLat, lng: initLng}}
             style={style}>
- 
+        {
+            coordsArray.length > 0 && 
+            coordsArray.map((item, idx) => 
+                <Marker key={`${item}${idx}`} position={item.coords} />
+            )
+        }
         {/* <Marker position={{lat: 37.778519, lng: -122.405640}} /> */}
-      </Map>
+        </Map>}
+        </>
     )
 }
 
--- client/src/components/requestsmap/index.js
+++ client/src/components/requestsmap/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './RequestsMap.css'
 import MapContainer from './components/MapContainer'
+import { requests } from '../../seedRequests.json'
 
 const RequestsMap = () => {
+    
+    const pinsArr = requests.filter(item => item.status === 'pending')
     return (
         <div className="requests-map-div card has-text-centered">
             <h2 className="title has-text-centered">Nearby Requests</h2>
             <div className="map-component-wrapper">
-                <MapContainer/>
+                <MapContainer pinsArr={pinsArr}/>
             </div>
         </div>
     )
